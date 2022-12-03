7354fe244b9f93c9605d3cde9c1d6c3b426fa9cb
Cristoval Neo Sasono <38718029+CristovalNS@users.noreply.github.com>
2022-12-03 10:07:08 +0700

    JavaScript Practice 2: Decipher

---
+++ README.md
@@ -0,0 +1,16 @@
+Made by: L1BC_Cristoval Neo Sasono_2602158235
+
+# Running the Code
+
+This JavaScript code can be run  directly through the computer terminal or the terminal within an IDE like VSC after downloading the extension 'Code Runner'.
+
+[IMPORTANT: Make sure that Node.js is installed in your device.]
+
+Once node is installed, open your terminal and type: node [file address]
+
+# Self Test 
+
+My result from the terminal:
+
+![Space N = 1000](self_test_img/Practice_2_Decipher.png)
+
---
+++ decipher.js
@@ -0,0 +1,21 @@
+function digitalDecipher(eMessage, key){
+	for(let i = 0; i < eMessage.length; i++){
+        key_index = i % key.toString().length;
+		eMessage[i] -= parseInt(key.toString()[key_index]);
+        // console.log(key_index); // Testing key index    
+	}
+
+    // console.log(eMessage); // Testing deciphered eMessage numbers
+	const alpha = '0abcdefghijklmnopqrstuvwxyz';
+
+	let decipher = '';
+
+	for(let i = 0; i < eMessage.length; i++){
+		decipher = decipher.concat(alpha[eMessage[i]]);
+	}
+	return decipher;
+}
+
+console.log("Testing output result:")
+console.log("Code [20, 12, 18, 30, 21] with key 1939:", digitalDecipher([20, 12, 18, 30, 21], 1939));
+console.log("Code [14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8] with key 1939:", digitalDecipher([14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8], 1939));
